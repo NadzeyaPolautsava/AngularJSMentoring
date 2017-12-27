e9506b5d7f92216f30ef31a7b5440192ddcfd9ac
Nadzeya Polautsava <Nadzeya_Polautsava@epam.com>
2017-12-27 15:10:43 +0300

    fixed bug

--- src/app/course/directives/highlightBorder.directive.ts
+++ src/app/course/directives/highlightBorder.directive.ts
@@ -19,8 +19,9 @@ export class HighlightBorderDirective {
       if (!this.courseCreatedDate) {
         return;
       }
-      let today: Date = new Date();
-      let twoWeeksBefore = new Date(today.getDate() - 14);
+      let today = new Date();
+      let twoWeeksBefore = new Date();
+      twoWeeksBefore.setDate(today.getDate() - 14);
       if (this.courseCreatedDate < today && this.courseCreatedDate >= twoWeeksBefore) {
         this.el.nativeElement.style.backgroundColor = 'lightgreen	';  
       } else if (this.courseCreatedDate > today) {
