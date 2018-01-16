b8e33e03a8e6616f2c2a4c173db888399cab9f97
Nadzeya Polautsava <Nadzeya_Polautsava@epam.com>
2018-01-16 15:39:01 +0300

    implemented get list of courses, delete course

--- src/app/core/services/course.service.ts
+++ src/app/core/services/course.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { ICourse } from './../../interfaces/course';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
@@ -11,51 +11,38 @@ export class CourseService {
 
     public courses: ICourse[] = [
         { id: 1, duration: 100, title: 'Some Video', date: new Date("February 4, 2022 10:13:00"), description: 'description', topRated: true },
-        { id: 2, duration: 200, title: 'Some Video2', date: new Date("February 4, 2016 10:13:00"), description: 'description2', topRated : true },
-        { id: 3, duration: 15, title: 'Some Video3', date: new Date("February 4, 2016 10:13:00"), description: 'description2', topRated : true },
-        
+        { id: 2, duration: 200, title: 'Some Video2', date: new Date("February 4, 2016 10:13:00"), description: 'description2', topRated: true },
+        { id: 3, duration: 15, title: 'Some Video3', date: new Date("February 4, 2016 10:13:00"), description: 'description2', topRated: true },
+
     ];
 
     public baseUrl: string;
 
-    constructor(private http: HttpClient) { 
+    constructor(private http: Http) {
         this.baseUrl = 'http://localhost:3004';
     }
 
     public getList(): Observable<ICourse[]> {
+        let requestOptions = new RequestOptions();
+        let request: Request;
 
-        return this.http.get<ICourse[]>('http://localhost:3004/courses');
-        // return this.http.get<ICourse[]>('http://localhost:3004/courses');
-        // console.log('in getList');
-        // let requestOptions = new RequestOptions();
-        // let request: Request;
-        // // let urlParams: URLSearchParams = new URLSearchParams();
+        requestOptions.url = this.baseUrl + '/courses';
+        requestOptions.method = RequestMethod.Get;
 
-        // requestOptions.url = this.baseUrl + '/courses';
-        // requestOptions.method = RequestMethod.Get;
-        // request = new Request(requestOptions);
-        // this.http.get('http://localhost:3004/courses').subscribe(data => {
-        //     // Read the result field from the JSON response.
-        //     console.log(data);
-        //   });
-        // return Observable.from(this.courses);
-        // this.http.request(request).subscribe( data => {
-        //     console.log(data);
-        // });
-        // let res: Observable<ICourse> = this.http.request(request)
-        //     .map((res:Response) => res.json());
-        
+        request = new Request(requestOptions);
+        return this.http.request(request)
+            .map((res: Response) => res.json());
     }
 
     public createCourse(duration: number, title: string, createddate: Date, description: string): Observable<ICourse> {
         let newCourseId = Math.max.apply(Math, this.courses.map(function (o) { o => o.id })) + 1;
-        let c: ICourse = { 
-            id: newCourseId, 
-            duration: duration, 
-            title: title, 
-            date: createddate, 
+        let c: ICourse = {
+            id: newCourseId,
+            duration: duration,
+            title: title,
+            date: createddate,
             topRated: false,
-            description: description 
+            description: description
         };
         this.courses.push(c);
         return Observable.of(c);
@@ -76,10 +63,30 @@ export class CourseService {
     }
 
     public removeItem(courseId: number) {
-        for (let i = 0; i < this.courses.length; ++i) {
-            if (this.courses[i].id == courseId) {
-                this.courses.splice(i, 1);
-            }
-        }
+        // for (let i = 0; i < this.courses.length; ++i) {
+        //     if (this.courses[i].id == courseId) {
+        //         this.courses.splice(i, 1);
+        //     }
+        // }
+        let requestOptions = new RequestOptions();
+        let request: Request;
+        // let urlParams: URLSearchParams = new URLSearchParams();
+        // let
+        requestOptions.url = this.baseUrl + '/courses/' + courseId;
+        requestOptions.method = RequestMethod.Delete;
+        // urlParams.append('id', courseId.toString());
+        // requestOptions.search = urlParams;
+        // requestOptions.body = '/' + courseId;
+
+        request = new Request(requestOptions);
+        return this.http.request(request)
+            .map((res: Response) => res.json());
+            // .subscribe(
+            //     x => console.log(x),
+            //     e => console.log(e),
+            //     () => {
+
+            //     }
+            // );
     }
 }
--- src/app/course/coursesPage/courseList/courseList.ts
+++ src/app/course/coursesPage/courseList/courseList.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, Input, OnDestroy } from '@angular/core'
 import { ICourse } from './../../../interfaces/course';
-import { CourseService } from  './../../../core/services/course.service';
+import { CourseService } from './../../../core/services/course.service';
 import { TitlePipe } from './../../../shared/pipes/title.pipe';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/map'
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'course-list',
   providers: [],
-  templateUrl: './courseList.html', 
+  templateUrl: './courseList.html',
   styleUrls: ['./courseList.css']
 })
-export class CourseListComponent implements OnInit {
+export class CourseListComponent implements OnInit, OnDestroy {
 
   public courses: ICourse[] = [];
+  private courseListSubscription: Subscription;
 
   constructor(private _courseService: CourseService) {
   }
@@ -25,9 +27,14 @@ export class CourseListComponent implements OnInit {
   }
 
   deleteItem($event) {
-    this._courseService.removeItem($event.value);
-    this.courses = [];
-    this.fetchCourses();
+    let deleteSubscription = this._courseService.removeItem($event.value)
+      .subscribe(
+        x => console.log(x),
+        e => console.log(e),
+        () => {
+          deleteSubscription.unsubscribe(); // is it okay???????
+          this.fetchCourses();
+      });
   }
 
   filter(title: string) {
@@ -35,29 +42,29 @@ export class CourseListComponent implements OnInit {
   }
 
   fetchCourses(): void {
+    if (this.courseListSubscription) {
+      this.courseListSubscription.unsubscribe();
+    }
     let today = new Date();
     let twoWeeksBefore = new Date();
-    
+
     twoWeeksBefore.setDate(today.getDate() - 14);
     console.log('twoWeeksBefore: ' + twoWeeksBefore);
-    // let subscription = this._courseService.getList().filter(x => x.date > twoWeeksBefore)
-    // .map(x => x as ICourse)
-    // .subscribe(
-    //     x => this.courses.push(x), 
-    //     e => console.log(e), 
-    //     () => console.log('data received')
-    // );
     this._courseService.getList()
-    .map(courses => courses.filter(course => ( course.topRated ) ))
-    .subscribe(
-        x => this.courses = x, 
-        e => console.log(e), 
-        () => console.log('data received')
-    );
-    console.log('NJNJJJ ');
-    for (let i = 0; i < this.courses.length; i++) {
-      console.log('Time: ' + this.courses[i].date.getTime());
+      .map(courses => courses.filter(course => (course.topRated)))
+      .subscribe(
+      x => {
+        this.courses = x;
+        console.log('Length: ' + x.length)
+      },
+      e => console.log(e),
+      () => console.log('data received')
+      );
+  }
+
+  ngOnDestroy() {
+    if (this.courseListSubscription) {
+      this.courseListSubscription.unsubscribe();
     }
-    // subscription.unsubscribe();
   }
 }
